255324d2b9afadd3820e2d9259ffb60a5c386a0d
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-05-23 10:19:06 +0100

    j

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,38 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <ImageBackground
+      source={require("../../assets/images/backgroundColor.png")}
+      style={styles.background}
+      resizeMode="cover"
+      >
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>Welcome Back</Text>
+        <Text style={styles.subTitle}>Join HomeConnect NG today</Text>
+      </View>
+     
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
+    height:'58%'
+  },
+
+  container: {
+    marginTop:70,
     alignItems: "center",
     justifyContent: "center",
   },
+  welcomeText:{
+    color:'white'
+  },
+  subTitle:{
+    color:'white',
+    marginTop:10
+  }
 });
